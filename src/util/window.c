76008765953bf12c7584fea825e7c1efb5aa13c8
Tencianity <arendechabert@gmail.com>
2026-03-21 21:24:28 -0400

    Initialized GUI

--- src/util/window.c
+++ src/util/window.c
@@ -236,6 +236,7 @@ int runWindow(int width, int height) {
     
     HudBox hudBox = initHudBox(window, renderer, width, height);
     SceneBox sceneBox = initSceneBox(window, renderer, width, height);
+    initUI();
    
     Uint32 lastTime = SDL_GetTicks();
     int running = 1;
